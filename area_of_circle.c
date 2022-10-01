518cf24492c27093fd49293f9e796d20f7156db4
basit67 <39161391+basit67@users.noreply.github.com>
2022-10-01 23:42:29 +0500

    Add files via upload

---
+++ area_of_circle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int r;
+    float pi = 3.14;
+    printf("what is radius of circle\n");
+    scanf("%d",&r);
+    printf("this is the area of cycle %f \n", pi*r*r);
+    return 0;
+}
