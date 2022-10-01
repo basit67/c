e5e87495057d091b44ab4872c0e10817fd7b0b8e
basit67 <39161391+basit67@users.noreply.github.com>
2022-10-01 23:30:13 +0500

    Add files via upload

---
+++ area_of_reacngle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+     int length, breath;
+    //  int area = ;
+    printf("enter length of reactangle \n");
+    scanf("%d", &length);
+    printf("enter breath of reactangle \n");
+    scanf("%d", &breath);
+    printf("this is area of reactangle\n", length*breath);
+ return 0;
+
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio. h>
+int main(){
+/t
+/treturn 0
+
+}
